b6a664c0a21e672a3ab782ecb0099cb906357b0d
song wonseok <misaka112@naver.com>
2024-08-05 15:51:35 +0900

    Advice 기본설정 기능 추가

--- src/main/java/com/tenco/bank/controller/MainController.java
+++ src/main/java/com/tenco/bank/controller/MainController.java
@@ -2,6 +2,7 @@ package com.tenco.bank.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // Ioc 대상(싱글톤 패턴 관리가 된다.) --> 제어의 역전
 public class MainController {
@@ -10,9 +11,9 @@ public class MainController {
 	
 	// 주소설계
 	// http:localhost:8080/main-page
-	@GetMapping("/main-page")
+	@GetMapping({"/main-page","/index"})
+	@ResponseBody
 	public String mainPage() {
-		
 		System.out.println("mainPage() 호출 확인");
 		// [JSP 파일 찾기 (yml 설정)] - 뷰 리졸버
 		// prefix: /WEB-INF/view
