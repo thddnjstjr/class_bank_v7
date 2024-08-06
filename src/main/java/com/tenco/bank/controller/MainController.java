626371122c379ffa991ac12315df481058c312a9
song wonseok <misaka112@naver.com>
2024-08-06 11:15:29 +0900

    에러 페이지 완료

--- src/main/java/com/tenco/bank/controller/MainController.java
+++ src/main/java/com/tenco/bank/controller/MainController.java
@@ -1,8 +1,17 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.handler.exception.RedirectException;
+import com.tenco.bank.handler.exception.UnAuthorizedException;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller // Ioc 대상(싱글톤 패턴 관리가 된다.) --> 제어의 역전
 public class MainController {
@@ -21,4 +30,36 @@ public class MainController {
 		// suffix: .jsp
 		return "/main";
 	}
+	
+	// todo - 삭제 예정
+	// 주소 설계
+	// http://localhost:8080/error-test1/true
+	// http://localhost:8080/error-test1/false
+	
+	@GetMapping("/error-test1")
+	public String errorPage() {
+		System.out.println("1111111111111");
+		if(true) {
+			throw new RedirectException("잘못된 요청입니다.", HttpStatus.BAD_REQUEST);
+		}
+		return "main";
+	}
+	
+	// http://localhost:8080/error-test2
+	@GetMapping("/error-test2")
+	public String errorData2() {
+		if(true) {
+			throw new DataDeliveryException("잘못된 데이터 입니다", HttpStatus.NOT_FOUND);
+		}
+		return "main";
+	}
+	
+	@GetMapping("/error-test3")
+	public String errorData3() {
+		if(true) {
+			throw new UnAuthorizedException("인증안된 사용자입니다", HttpStatus.NOT_FOUND);
+		}
+		return "main";
+	}
+	
 }
--- src/main/java/com/tenco/bank/handler/exception/RedirectException.java
+++ src/main/java/com/tenco/bank/handler/exception/RedirectException.java
@@ -11,7 +11,7 @@ public class RedirectException extends RuntimeException{
 	
 	private HttpStatus status;
 	
-	public RedirectException(String message,HttpStatus status) {
+	public RedirectException(String message, HttpStatus status) {
 		super(message);
 		this.status = status;
 	}
